eaea24138fda064aab67dcf58aa1192f1372b6c8
*Ethan <*egmotta88@gmail.com*>
2025-03-29 17:24:10 -0700

    Added a line to hello.py and README

---
+++ my_first_repo/README.md
@@ -0,0 +1,5 @@
+# My First Repop
+
+This is my first repo to be uploaded to GitHub. 
+It contains three files, a Python script, a README.md file and a picture of my cat when he was a kitten.
+Added this line for the second commit to a different file.
--- my_first_repo/hello.py
+++ my_first_repo/hello.py
@@ -3,6 +3,7 @@ Ethan Motta
 Module 08 Lab Assignment Part B
 This file is apart of my first time creating a repo.
 It simply just prints Hello World! The classic programming starter code.
+Added this line for the second commit.
 """
 
 print("Hello World!")
