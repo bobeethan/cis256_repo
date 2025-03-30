cc259665beccaa213ab2a0afa93d67add111da8b
*Ethan <*egmotta88@gmail.com*>
2025-03-29 17:12:53 -0700

    first commit

---
+++ my_first_repo/hello.py
@@ -0,0 +1,8 @@
+"""
+Ethan Motta
+Module 08 Lab Assignment Part B
+This file is apart of my first time creating a repo.
+It simply just prints Hello World! The classic programming starter code.
+"""
+
+print("Hello World!")
